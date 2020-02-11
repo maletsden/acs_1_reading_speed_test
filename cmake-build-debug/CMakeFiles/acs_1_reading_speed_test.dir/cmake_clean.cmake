baf0b08d96d0ab23fbe9970ce30898d40641f4d4
Denys Maletskiy <maletsden@gmail.com>
2020-02-11 12:28:17 +0200

    Basic CLion project

---
+++ cmake-build-debug/CMakeFiles/acs_1_reading_speed_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/acs_1_reading_speed_test.dir/main.cpp.o"
+  "acs_1_reading_speed_test.pdb"
+  "acs_1_reading_speed_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/acs_1_reading_speed_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
