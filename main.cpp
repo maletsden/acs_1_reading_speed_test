0d2fd67a48f7b057369adabe01efd743bad97610
Denys <maletsden@gmail.com>
2020-02-13 01:05:29 +0200

    Merged

--- main.cpp
+++ main.cpp
@@ -3,36 +3,72 @@
 #include <limits>
 #include <vector>
 #include <cctype>
+#include <string>
+#include <sstream>
 
-void print(std::vector<char> const &input)
-{
-    for (int i = 0; i < input.size(); i++) {
-        std::cout << input.at(i);
+void print(const std::string &input) {
+    for (char chr : input) {
+        std::cout << chr;
     }
+
+    std::cout << std::endl;
 }
 
-void file_is_opened(const std::fstream& in){
+void print_deque_of_strings(const std::deque<std::string>& mydeque) {
+    for (auto & it : mydeque) {
+        print(it);
+    }
+}
+void file_is_opened(const std::fstream& in) {
     if (!in.is_open()){
         throw std::ios_base::failure{"Error. File is closed."};
     }
 }
 
 
-int num_not_ws(std::vector<char> const &container){
+int num_not_ws(const std::vector<char> &container) {
     int num = 0;
-    for (int i = 0; i < container.size(); ++i){
-        if (!isspace(container.at(i))){
+    for (char chr : container) {
+        if (!isspace(chr)) {
             ++num;
         }
     }
     return num;
-
 }
 
+std::string read_stream_into_string(const std::fstream& in) {
+    file_is_opened(in);
 
-auto read_file_into_vector(std::fstream& in)
-{
+    std::ostringstream ss;
 
+    if (!(ss << in.rdbuf()))
+        throw std::ios_base::failure{"error"};
+
+    return ss.str();
+}
+
+std::deque<std::string> read_file_into_deque(std::fstream& in) {
+    file_is_opened(in);
+
+    auto const chunk_size = std::size_t{BUFSIZ};
+
+    std::deque<std::string> container;
+
+    std::string chunk(chunk_size, '\0');
+
+    while (
+        in.read(&chunk[0], chunk_size) ||
+        in.gcount()
+    )
+        container.insert(
+            end(container),
+            chunk
+        );
+
+    return container;
+}
+
+auto read_file_into_vector(std::fstream& in) {
     file_is_opened(in);
 
     auto const start_pos = in.tellg();
@@ -40,10 +76,10 @@ auto read_file_into_vector(std::fstream& in)
         throw std::ios_base::failure{"error"};
     }
 
-    if (!in.ignore(
-            std::numeric_limits<std::streamsize>::max())){
+    if (!in.ignore(std::numeric_limits<std::streamsize>::max())) {
         throw std::ios_base::failure{"error"};
     }
+
     auto const char_count = in.gcount();
 
     if (!in.seekg(start_pos)){
@@ -52,8 +88,7 @@ auto read_file_into_vector(std::fstream& in)
 
     std::vector<char> container(char_count);
 
-    if (0 != container.size())
-    {
+    if (!container.empty()) {
         if (!in.read(&container[0], container.size())){
             throw std::ios_base::failure{"error"};
         }
@@ -65,10 +100,25 @@ auto read_file_into_vector(std::fstream& in)
 
 int main() {
     std::fstream in;
-    in.open("test_small.txt", std::fstream::in);
+    in.open("test.txt", std::fstream::in);
 
-    std::vector<char> container = read_file_into_memory(in);
+    // First method
+    std::string s = read_stream_into_string(in);
+    print(s);
 
+    // return file pointer to the beginning
+    in.seekg (0, std::fstream::beg);
+
+    // Second method
+    std::vector<char> container = read_file_into_vector(in);
+
+    // return file pointer to the beginning
+    in.seekg (0, std::fstream::beg);
+
+    // Third method
+    std::deque<std::string> mydeque = read_file_into_deque(in);
+
+    print_deque_of_strings(mydeque);
 
     in.close();
     return 0;
